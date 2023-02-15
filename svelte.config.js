152d9b71ca1d114bbe89b4c93621cba7946e53fc
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2023-02-15 19:25:18 +0300

    ready to deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
+	paths: {
+		base: dev ? '' : '/1c-to-sbis',
+	  },
 	kit: {
 		adapter: adapter()
 	}
