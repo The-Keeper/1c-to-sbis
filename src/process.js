f8686fbfc049c2a0384eade7082d15cd2a2315b6
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2023-07-26 16:14:34 +0500

    utils and format version update

--- src/process.js
+++ src/process.js
@@ -1,15 +1,18 @@
-import { add_col_aoa, del_col_aoa, parseNum } from "./utils";
+import { add_col_aoa, del_col_aoa, parseNum, aoa_ColumnSpliceBySignature, aoa_CopyColumnBySignature, aoa_EnumerateDataBySignature } from "./utils";
+
 
 export const schema = [
     {
-        id: '5.09', title_cells: ["A2"],
+        id: '5.09', title_cells:`  nj` ["A2"],
         buy: { output: '0000080 5_09 Книга покупок.xls', 'del-rows': [11, 9], 'del-cols': ['O'] },
         sell: { output: '0000090 5_09 Книга продаж.xls', 'del-rows': [11, 9], 'del-cols': ['AU', 'AT', 'AR', 'AP', 'AN', 'AL', 'AJ', 'AH', 'AF', 'AD', 'AB', 'Z', 'X', 'V', 'T', 'R', 'P', 'N', 'L', 'I', 'H', 'F', 'D'] }
     },
     {
         id: '5.10', title_cells: ["A2", "G1", "H1"],
         buy: {
-            output: '0000080 5_10 Книга покупок.xls', process: (aoa) => 
+            output: '0000080 5_10 Книга покупок.xls', 
+            // PREFERABLE "process" workflow: process, splice columns, splice rows
+            process: (aoa) => 
             {
                 aoa.splice(1, 0, [""]);     // insert one empty row
                 aoa.splice(8, 2)            // delete two rows after 8th
@@ -39,6 +42,39 @@ export const schema = [
                 }
             }
         },
-        sell: { output: '0000090 5_10 Книга продаж.xls', }
+        sell: { output: '0000090 5_10 Книга продаж.xls', 
+        // PREFERABLE "process" workflow: process, splice columns, splice rows
+            process: (aoa) => {
+                const DATA_IS_PRESENT_COL = 0
+
+                // меняем формат ИИН и КПП
+                const IIN_COL = 6, KPP_COL = 7; 
+                const DATA_STARTS_ROW = 11; 
+                for (let r = DATA_STARTS_ROW; r < aoa.length; r++) {
+                    if (aoa[r][DATA_IS_PRESENT_COL] != "") {
+                        aoa[r][IIN_COL] = aoa[r][IIN_COL] + "/" + aoa[r][KPP_COL]
+                    }
+                }
+                const ENUM_ROW = 10;
+                aoa_ColumnSpliceBySignature(aoa, ENUM_ROW, "3а", 1, 4);     // удалить 3а и добавить 4 колонки на её месте
+                aoa_ColumnSpliceBySignature(aoa, ENUM_ROW, "5а", 0, 7);     // вставить 7 колонок перед пунктом 5а
+                aoa_CopyColumnBySignature(aoa, ENUM_ROW, "8а", "row-13", DATA_STARTS_ROW); // расставляем колонки по местам
+                aoa_CopyColumnBySignature(aoa, ENUM_ROW, "5а", "row-14", DATA_STARTS_ROW);
+                aoa_CopyColumnBySignature(aoa, ENUM_ROW, "6а", "row-15", DATA_STARTS_ROW);
+                aoa_CopyColumnBySignature(aoa, ENUM_ROW, "7",  "row-16", DATA_STARTS_ROW);
+                aoa_CopyColumnBySignature(aoa, ENUM_ROW, "8б", "row-17", DATA_STARTS_ROW);
+                aoa_CopyColumnBySignature(aoa, ENUM_ROW, "5б", "row-18", DATA_STARTS_ROW);
+                aoa_CopyColumnBySignature(aoa, ENUM_ROW, "6б", "row-19", DATA_STARTS_ROW);
+                aoa_ColumnSpliceBySignature(aoa, ENUM_ROW, "5а", 0, 5);     // добавить ещё 5 колонок
+                aoa_ColumnSpliceBySignature(aoa, ENUM_ROW, "1", 0, 2, "Продажа", "Руководитель");  // сдвинуть всё между "Продажа" и "Руководитель" на две колонки
+                aoa_EnumerateDataBySignature(aoa, ENUM_ROW, "row-0", "1", DATA_STARTS_ROW);  // добавить нумерацию строк
+                const OUTPUT_COL_NUM = 27
+                for (let i = 0; i < OUTPUT_COL_NUM; i++) {  // добавить нумерацию столбцов
+                    aoa[ENUM_ROW][i] = i+1;    
+                }
+                aoa.splice(2, 0, [""]);     // добавить пустую сторку перед третьей
+                aoa.splice(8, 2)            // удалить две строки после 8-й
+            } 
+        }
     }
 ];
--- src/utils.js
+++ src/utils.js
@@ -62,3 +62,98 @@ export function parseNum(s) {
     }
     return 0;
 }
+
+/**
+ * Enumerates rows into given column by provided signatures
+ * @param {Array<any>} aoa - array-of-arrays to perform the action on
+ * @param {number} row - row to look for signatures in
+ * @param {string} enumerationColSignature - signature of column where to put enumeration
+ * @param {string} dataColSignature - signature of column with contiguous chunk of data
+ * @param {number} rowDataStart - row where data starts
+ */
+export function aoa_EnumerateDataBySignature(aoa, row, enumerationColSignature, dataColSignature, rowDataStart) {
+    let col_data = -1, col_to = -1; 
+    for (let c = 0; c < aoa[row].length; c++) {
+        if (aoa[row][c] == dataColSignature) 
+            col_data = c;
+        if (aoa[row][c] == enumerationColSignature) 
+            col_to = c;
+    }
+    if (col_data>=0 && col_to>=0 && col_data !=col_to) {
+        for (let i=0; rowDataStart + i < aoa.length; i++) {
+            let r = rowDataStart + i;
+            if (aoa[r][col_data] == "") 
+                break;
+            aoa[r][col_to] = i+1;
+            }
+        }
+}
+
+
+/**
+ * Copies data between columns based on signature
+ * @param {Array<any>} aoa - array-of-arrays to perform the action on
+ * @param {number} row - row to look for signatures in
+ * @param {string} fromColSignature - signature of column where to get data from
+ * @param {string} toColSignature - signature of column where to put data data
+ * @param {number} rowDataStart - row where data starts
+ */
+export function aoa_CopyColumnBySignature(aoa, row, fromColSignature, toColSignature, rowDataStart) {
+    let col_from = -1, col_to = -1; 
+    for (let c = 0; c < aoa[row].length; c++) {
+        if (aoa[row][c] == fromColSignature) 
+            col_from = c;
+        if (aoa[row][c] == toColSignature) 
+            col_to = c;
+        }
+    if (col_from>=0 && col_to>=0 && col_from !=col_to) {
+        for (let r = rowDataStart; r < aoa.length; r++) {
+            if (aoa[r][0] == "") 
+                break;
+            aoa[r][col_to] = aoa[r][col_from];
+            }
+        }
+}
+
+/**
+ * Splices the columns of array-of-arrays object based on value of a cell in a given row
+ * @param {Array<any>} aoa - array-of-arrays to perform the action on
+ * @param {number}  row - row to scan left-to-right
+ * @param {string} signature - string that the cell must contain to perform operations on
+ * @param {number} delCount - number of columns to delete
+ * @param {number} addCount - number of columns to insert
+ * @param {string} [startAfterSignature=""] - start moving columns after row that starts with given signature
+ * @param {string} [terminateBeforeSignature=""] - finish moving columns before row that ends with given signature
+ */
+export function aoa_ColumnSpliceBySignature (aoa, row, signature, delCount, addCount, startAfterSignature = "", terminateBeforeSignature = "") {   
+    for (let c = 0; c < aoa[row].length; c++) {
+        const partialMoveAllowed = startAfterSignature && terminateBeforeSignature && !(delCount>0);
+        if (aoa[row][c] == signature) {
+            const spliceArgs = [c, delCount].concat(Array(addCount).fill(""));
+            const additionArgs = [aoa[row].length, 0].concat(Array(addCount).fill(""));
+            const signAddArgs  = [c, delCount].concat([ ...Array(addCount).keys() ].map(  i => `row-${i+c}` ))
+
+            let partialMoveStarted = false;
+            for (let r=0; r < aoa.length; r++) {
+
+                if (partialMoveAllowed && aoa[r][0].startsWith(terminateBeforeSignature)) {
+                    partialMoveStarted = false;
+                }
+                if (partialMoveAllowed && !partialMoveStarted) {
+                    aoa[r].splice(...additionArgs);
+                } else                 
+                {
+                    if (r == row) {
+                        aoa[r].splice(...signAddArgs);                     
+                    } else {
+                        aoa[r].splice(...spliceArgs);
+                    }
+                } 
+                if (partialMoveAllowed && aoa[r][0].startsWith(startAfterSignature)) {
+                    partialMoveStarted = true;
+                }
+            }
+            break;
+        }
+    }
+}
