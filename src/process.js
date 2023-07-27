291122d7dd61ae26e2af511f73fb6b78e444ef69
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2023-07-27 15:50:54 +0500

    fix mistype

--- src/process.js
+++ src/process.js
@@ -3,7 +3,7 @@ import { add_col_aoa, del_col_aoa, parseNum, aoa_ColumnSpliceBySignature, aoa_Co
 
 export const schema = [
     {
-        id: '5.09', title_cells:`  nj` ["A2"],
+        id: '5.09', title_cells: ["A2"],
         buy: { output: '0000080 5_09 Книга покупок.xls', 'del-rows': [11, 9], 'del-cols': ['O'] },
         sell: { output: '0000090 5_09 Книга продаж.xls', 'del-rows': [11, 9], 'del-cols': ['AU', 'AT', 'AR', 'AP', 'AN', 'AL', 'AJ', 'AH', 'AF', 'AD', 'AB', 'Z', 'X', 'V', 'T', 'R', 'P', 'N', 'L', 'I', 'H', 'F', 'D'] }
     },
